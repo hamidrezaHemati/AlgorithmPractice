7dcf3eb36014e616d9ccc856ff2a940b462c5d87
unknown <hrhhemati@gmail.com>
2024-05-24 16:41:22 +0330

    getting input

--- Q1_Dictator/main.cpp
+++ Q1_Dictator/main.cpp
@@ -4,6 +4,15 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	cout << "hello world";
+	int n, m;
+	cin >> n >> m;
+	int nodes = n; int edges = m;
+	int weight, x_node, y_node;
+	for(int i=0; i<edges; i++){
+		cin >> x_node >> y_node >> weight;
+	}
+	cout<<"checking"<<endl;
+	cout<<"number of nodes: "<<nodes<<" number of edges: "<<edges<<endl;
+	cout<<x_node<<weight;
 	return 0;
 }
