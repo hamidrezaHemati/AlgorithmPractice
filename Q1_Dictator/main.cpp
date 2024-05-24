b22d01dc077393f786b85fb25510054a185b07d6
unknown <hrhhemati@gmail.com>
2024-05-24 17:01:50 +0330

    adding struct data structure to the code for saving edges inputs

--- Q1_Dictator/main.cpp
+++ Q1_Dictator/main.cpp
@@ -2,17 +2,24 @@
 
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+struct Edge{
+	int x, y, w;
+};
+
 
 int main(int argc, char** argv) {
 	int n, m;
 	cin >> n >> m;
-	int nodes = n; int edges = m;
-	int weight, x_node, y_node;
-	for(int i=0; i<edges; i++){
-		cin >> x_node >> y_node >> weight;
+	int number_of_nodes = n; int number_of_edges = m;
+	struct Edge edges[number_of_edges];
+	for(int i=0; i<number_of_edges; i++){
+		cin >> edges[i].x >> edges[i].y >> edges[i].w;
 	}
 	cout<<"checking"<<endl;
-	cout<<"number of nodes: "<<nodes<<" number of edges: "<<edges<<endl;
-	cout<<x_node<<weight;
+	cout<<"number of nodes: "<<number_of_nodes<<" number of edges: "<<number_of_edges<<endl;
+	cout<<"number of edges based on sizof: "<<sizeof(edges)/sizeof(struct Edge)<<endl;
+	for(int i=0; i<number_of_edges; i++){
+		cout<<edges[i].x << edges[i].y << edges[i].w;
+	}
 	return 0;
 }
